2c7aef0aa49094b9e81b288db9200c5c2ca759a7
Sven Lehmann <sven.lehmann93@gmail.com>
2018-05-15 11:59:28 +0200

    Refactoring SvedeB entry point

--- src/main/scala/de/hpi/svedeb/SvedeB.scala
+++ src/main/scala/de/hpi/svedeb/SvedeB.scala
@@ -1,27 +1,13 @@
 package de.hpi.svedeb
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import de.hpi.svedeb.SvedeB.{GetAPI, ReturnedAPI, Shutdown}
+import akka.actor.{ActorRef, ActorSystem}
 import de.hpi.svedeb.api.API
 import de.hpi.svedeb.management.TableManager
 
 object SvedeB {
-
-  case class Shutdown()
-  case class GetAPI()
-
-  case class ReturnedAPI(api: ActorRef)
-
-  def props(): Props = Props(new SvedeB())
-}
-
-class SvedeB extends Actor with ActorLogging {
-
-  private val tableManager = context.actorOf(TableManager.props())
-  private val api = context.actorOf(API.props(tableManager))
-
-  override def receive: Receive = {
-    case Shutdown() => ???
-    case GetAPI() => sender() ! ReturnedAPI(api)
+  def start(): ActorRef = {
+    val system = ActorSystem("SvedeB")
+    val tableManager = system.actorOf(TableManager.props())
+    system.actorOf(API.props(tableManager))
   }
 }
