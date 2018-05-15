488b45151808904425acbf45b449d7a33dad1b5d
Sven Lehmann <sven.lehmann93@gmail.com>
2018-05-16 01:37:49 +0200

    refactoring ExternalUser

--- src/test/scala/de/hpi/svedeb/ExternalUser.scala
+++ src/test/scala/de/hpi/svedeb/ExternalUser.scala
@@ -10,6 +10,7 @@ import de.hpi.svedeb.queryplan.QueryPlan._
 import de.hpi.svedeb.table.RowType
 import org.scalatest.Matchers._
 
+import scala.annotation.tailrec
 import scala.concurrent.duration._
 
 object ExternalUser extends App {
@@ -17,11 +18,23 @@ object ExternalUser extends App {
 
   implicit val timeout: Timeout = Timeout(30 seconds)
 
+  def createInsertQuery(tableName: String, depth: Int): QueryPlanNode = {
+    @tailrec
+    def iter(depth: Int, queryPlanNode: QueryPlanNode): QueryPlanNode = {
+      depth match {
+        case 0 => queryPlanNode
+        case d => iter(d - 1, InsertRow(queryPlanNode, RowType("a" + depth, "b" + depth)))
+      }
+    }
+    iter(depth, GetTable(tableName))
+  }
+
   def testMaterialize(): Unit = {
     val future1 = api.ask(Query(CreateTable("Table1", Seq("column1", "column2"), 100)))
     val result1 = Await.result(future1, timeout.duration).asInstanceOf[Result]
 
-    val future2 = api.ask(Query(createInsertQuery(1000)))
+    val queryPlanNode = createInsertQuery("Table1", 100)
+    val future2 = api.ask(Query(queryPlanNode))
     val insertedTable = Await.result(future2, timeout.duration).asInstanceOf[Result]
     val future3 = api.ask(Materialize(insertedTable.resultTable))
     val result3 = Await.result(future3, timeout.duration).asInstanceOf[MaterializedResult]
@@ -34,7 +47,7 @@ object ExternalUser extends App {
     println("finished creating")
   }
 
-  def testScan(): Unit = {
+  def testDoubleScan(): Unit = {
     // Scan table
     val future4 = api.ask(Query(Scan(Scan(GetTable("Table1"), "column1", x => x.contains("1")), "column2", x => x.contains("2"))))
     val result4 = Await.result(future4, timeout.duration).asInstanceOf[Result]
@@ -50,13 +63,27 @@ object ExternalUser extends App {
     println("finished")
   }
 
-  def createInsertQuery(depth: Int): QueryPlanNode = {
-    if (depth == 0) return GetTable("Table1")
-    InsertRow(createInsertQuery(depth - 1), RowType("a" + depth, "b" + depth))
+  def testScan(): Unit = {
+    // Scan table
+    val queryFuture = api.ask(Query(Scan(GetTable("Table1"), "column1", x => x.contains("10"))))
+    val queryResult = Await.result(queryFuture, timeout.duration).asInstanceOf[Result]
+
+    val materializeFuture = api.ask(Materialize(queryResult.resultTable))
+    val materializedResult = Await.result(materializeFuture, timeout.duration).asInstanceOf[MaterializedResult]
+
+    println(materializedResult.result)
+
+    println("finished")
   }
 
   testMaterialize()
+  testDoubleScan()
+
+  val before = System.nanoTime()
   testScan()
+  val after = System.nanoTime()
+
+  println("Time for single scan: {}ms", (after - before)/1000000)
 
   api ! Shutdown()
 
