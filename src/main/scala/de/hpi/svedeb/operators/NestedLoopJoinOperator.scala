5914f1ccf17cd79a168b618917d6c30327e5e0ab
Unknown <frederike.ramin@student.hpi.de>
2018-05-28 15:36:59 +0200

    Add operator for nested loop join

---
+++ src/main/scala/de/hpi/svedeb/operators/NestedLoopJoinOperator.scala
@@ -0,0 +1,43 @@
+package de.hpi.svedeb.operators
+
+import akka.actor.{ActorRef, Props}
+import de.hpi.svedeb.operators.AbstractOperator.Execute
+import de.hpi.svedeb.operators.NestedLoopJoinOperator.JoinState
+import de.hpi.svedeb.operators.workers.ScanWorker.{ScanJob, State}
+import de.hpi.svedeb.table.Column.{FilteredRowIndices, ScannedValues}
+import de.hpi.svedeb.table.Partition.ColumnsRetrieved
+import de.hpi.svedeb.table.Table.PartitionsInTable
+
+object NestedLoopJoinOperator {
+  def props(leftTable: ActorRef, rightTable: ActorRef): Props = Props(new NestedLoopJoinOperator(leftTable, rightTable))
+
+  private case class JoinState(originalSender: ActorRef)
+}
+
+class NestedLoopJoinOperator(leftTable: ActorRef, rightTable: ActorRef) extends AbstractOperator {
+  override def receive: Receive = active(JoinState(ActorRef.noSender))
+
+  def initializeJoin(): Unit = {
+    // fetch partitions
+    context.become(active(JoinState(sender())))
+  }
+
+  def handlePartitions(partitions: Seq[ActorRef]): Unit = {
+    // save once with sender
+    // invoke join jobs else
+    // save number of jobs, forward partition id
+  }
+
+  def handlePartialResult(partitionId: Int, partition: ActorRef): Unit = {
+    // store result
+    // if all received, create new table and return
+  }
+
+  private def active(state: JoinState): Receive = {
+    case Execute() => initializeJoin()
+    case PartitionsInTable(partitions) => handlePartitions(partitions)  // get partitions
+    case PartialResult(partitionId, partition) => handlePartialResult(partitionId, partition)
+    case m => throw new Exception(s"Message not understood: $m")
+  }
+}
+
