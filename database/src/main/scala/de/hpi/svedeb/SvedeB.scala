2dd2b017cf0fe9ef6294fa5c54c332bf6247e3c8
Unknown <frederike.ramin@student.hpi.de>
2018-06-25 15:16:10 +0200

    Add default to SvedeB constructor

--- database/src/main/scala/de/hpi/svedeb/SvedeB.scala
+++ database/src/main/scala/de/hpi/svedeb/SvedeB.scala
@@ -5,7 +5,7 @@ import de.hpi.svedeb.api.API
 import de.hpi.svedeb.management.TableManager
 
 object SvedeB {
-  def start(remoteAPIs: Seq[ActorRef], remoteTableManagers: Seq[ActorRef]): ActorRef = {
+  def start(remoteAPIs: Seq[ActorRef] = Seq.empty, remoteTableManagers: Seq[ActorRef] = Seq.empty): ActorRef = {
     val system = ActorSystem("SvedeB")
     val tableManager = system.actorOf(TableManager.props(remoteTableManagers))
     system.actorOf(API.props(tableManager, remoteAPIs))
